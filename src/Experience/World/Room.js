6e89f2593faca3dc19be98814dfe7d937cd016c1
zenithsui <zenithsuiyt@gmail.com>
2026-06-24 06:41:18 +0530

    Fix black screen: auto-fit camera to model bounding box

--- src/Experience/World/Room.js
+++ src/Experience/World/Room.js
@@ -9,18 +9,33 @@ export class Room {
   }
 
   init() {
-    // Center and scale the model to fit the scene
-    const box = new THREE.Box3().setFromObject(this.model);
+    const box    = new THREE.Box3().setFromObject(this.model);
     const center = box.getCenter(new THREE.Vector3());
+    const size   = box.getSize(new THREE.Vector3());
     this.model.position.sub(center);
-
     this.experience.scene.add(this.model);
+
+    // Auto-fit camera regardless of model scale
+    const cam     = this.experience.camera;
+    const maxDim  = Math.max(size.x, size.y, size.z);
+    const fov     = cam.instance.fov * (Math.PI / 180);
+    const dist    = (maxDim / 2) / Math.tan(fov / 2) * 2.2;
+    const targetY = size.y * 0.15;
+
+    cam.instance.position.set(0, targetY, dist);
+    cam.instance.lookAt(0, targetY, 0);
+    cam.basePosition.set(0, targetY, dist);
+    cam.homePosition.copy(cam.basePosition);
+    cam.baseRotation.set(0, 0, 0);
+
+    if (cam.controls) {
+      cam.controls.target.set(0, targetY, 0);
+      cam.controls.update();
+    }
   }
 
   toggleDayNight() {}
   resize() {}
   update() {}
-  destroy() {
-    this.experience.scene.remove(this.model);
-  }
+  destroy() { this.experience.scene.remove(this.model); }
 }
